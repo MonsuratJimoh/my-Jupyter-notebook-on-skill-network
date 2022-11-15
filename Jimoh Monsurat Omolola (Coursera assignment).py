ae82d3f1525b270127378ae738c0327454a67852
Engr. Dr. Monsurat Omolola Jimoh <111320308+MonsuratJimoh@users.noreply.github.com>
2022-11-15 16:58:37 +0100

    Add files via upload

---
+++ Jimoh Monsurat Omolola (Coursera assignment).py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <center>
+#     <img src="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DS0105EN-SkillsNetwork/labs/Module2/images/SN_web_lightmode.png" width="300" alt="cognitiveclass.ai logo"  />
+# </center>
+# 
+
+# <h1>My Jupyter Notebook on Skills Network Lab<h1>
+
+# **Jimoh Monsurat Omolola**
+# 
+# A researcher and an aspiring Data Scientist
+
+# *I am interested in data science because as a researcher, i need data science and machine learning skills to effectively analyze and interprete various experimental data*
+
+# <h3>The code in the next cell will perform simple addition<h3>
+
+# In[2]:
+
+
+x=2
+y=5
+print(x+y)
+
+
+# - I'm a Chemical Engineer
+# - I love data analysis
+# - I am currently working on my skills
+# 
+# **the skills include**
+# 
+# 1. Excel
+# 2. SQL
+# 3. Tableau
+# 4. Python
+# 5. Machine learning
+# 
+# 
+# Learning new skills may be overwhelming
+# 
+# ---
+# 
+# It's worth the stress !!!
+# 
+# 
+# ~I hate procastination~
+# 
+# 
+
+# In[ ]:
+
+
+
+
